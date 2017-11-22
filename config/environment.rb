2d129609d0f5a21cb9c80d483ee49bb52801dc48
tochman <thomas@craftacademy.se>
2017-11-22 09:26:51 +0100

    minor tweaks and code cleaning

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,7 @@
-# Set up gems listed in the Gemfile.
-# See: http://gembundler.com/bundler_setup.html
-#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-# Require gems we care about
 require 'rubygems'
 
 require 'uri'
@@ -24,15 +20,9 @@ require 'erb'
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = 'slow_food'
 
-
-# Some helper constants for path-centric logic
 require APP_ROOT.join('config', 'database')
 
 class SlowFoodApp < Sinatra::Base
-
-  # not_found do
-  #   raise RuntimeError
-  # end
   disable :logger, :dump_errors
   enable :sessions
   set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
